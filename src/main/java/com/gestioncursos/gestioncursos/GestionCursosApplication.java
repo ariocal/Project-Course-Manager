bcec0d4a5dfcc3198f50ca4b97dbaac4f9f59776
ariocal <126732667+ariocal@users.noreply.github.com>
2024-03-11 17:13:37 -0500

    Aplicacion finalizada

---
+++ src/main/java/com/gestioncursos/gestioncursos/GestionCursosApplication.java
@@ -0,0 +1,14 @@
+package com.gestioncursos.gestioncursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCursosApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(GestionCursosApplication.class, args);
+	}
+
+}
